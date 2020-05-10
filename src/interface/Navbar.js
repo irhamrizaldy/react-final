7fdaf3d69bb5d89b4b76ea6b7e3d2575a5595ff3
irhamrizaldy <rirham12@gmail.com>
2020-05-10 15:33:25 +0700

    minor fix

--- src/interface/Navbar.js
+++ src/interface/Navbar.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SearchField from 'react-search-field';
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper indigo accent-4">
             <div className="container">
                 <Link to="/" className="brand-logo">Shoeses</Link>
-
                 <ul className="right">
                     <li><Link to="/">Shop</Link></li>
                     <li><Link to="/cart">My Cart</Link></li>
-                    <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                 </ul>
             </div>
         </nav>
